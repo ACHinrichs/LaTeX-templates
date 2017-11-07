0b5d95da3ffe5e1ee36e9302f3961338e233e3ed
Adrian C. Hinrichs <adrian.hinrichs@rwth-aachen.de>
2017-11-07 13:47:37 +0100

    Fix onecolumn maketitle bug

---
+++ homeworkassignment/test.ex
@@ -0,0 +1,32 @@
+\documentclass[twoside]{HomeworkAssignment}
+\usepackage{lipsum}
+\author{Adrian C. Hinrichs Matr. 74656}
+\tutorium{\"Ubungsgruppe 42}
+\kurs{Analysis f\"ur Informatiker}
+\date{\today}
+\begin{document}
+\maketitle
+\newproblem
+\newsubproblem
+\lipsum[1]
+\newsubproblem
+\lipsum[2]
+\lipsum[3]
+\solution
+\lipsum[4]
+\solution[i]
+\lipsum[5]
+\subproblem{Schlussfolgerung}
+\newsubsubproblem
+\lipsum[1]
+\newsubsubproblem
+\lipsum[2]
+\newproblem
+\lipsum[4]
+\proof
+\lipsum[5]
+\problem{Bonusaufgabe}
+\lipsum[6]
+\proof[durch vollst\"andige Induktion]
+\lipsum[7]
+\end{document}
